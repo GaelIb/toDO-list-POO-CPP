4960199c4a1d213286cc7cbbc3be7b4c2f060735
GaelIb <ibayezgael@gmail.com>
2025-06-09 23:35:25 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,34 +3,100 @@
 #include "Project.h"
 #include "Task.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    Admin admin("GaelIbanez", "1234567890", "gael@gmail.com");
-    Collaborator collab("Pedro", "0987654321", "pedrito@email.com");
+    vector<Admin> admins;
+    vector<Collaborator> collaborators;
+    vector<Project> projects;
 
-    Project project("Signus Challengue", "2025-06-30");
-    project.addMember(admin);
-    project.addMember(collab);
+    int op = -1;
 
-    Task task("Create a Presentation", "Write a pitch", "Pedro", 2);
-    project.addTask(task);
+    while (op != 0) {
+        cout << "1. Add admin\n";
+        cout << "2. Add collaborator\n";
+        cout << "3. Create project\n";
+        cout << "4. Add admin or collaborator to the project\n";
+        cout << "5. Add task to project\n";
+        cout << "6. Show all \n";
+        cout << "0. Exit\n";
+        cin >> op;
 
-    // Modificar nombre del proyecto usando setter
-    project.setName("Signus Challenge(Updated)");
-
-    // Cambiar el estado de la tarea usando Collaborator
-    collab.changeTaskState((Task&)project.getTasks()[0], "Started");
-
-    // Mostrar resumen
-    cout << "Project: " << project.getName() << ", Due: " << project.getDueDate() << endl;
-    cout << "Members: ";
-    for (const User* u : project.getMembers())
-        cout << u->getName() << " | ";
-    cout << endl;
-    cout << "Tasks:\n";
-    for (const Task& t : project.getTasks()) {
-        cout << "- " << t.getName() << " (" << t.getState() << "), Assigned to: " << t.getAssignedTo() << endl;
+        if (op == 1) {
+            string n, p, e;
+            cout << "Admin name: "; cin >> n;
+            cout << "Phone: "; cin >> p;
+            cout << "Email: "; cin >> e;
+            admins.push_back(Admin(n, p, e));
+            cout << "Admin added\n";
+        }
+        else if (op == 2) {
+            string n, p, e;
+            cout << "Collaborator name: "; cin >> n;
+            cout << "Phone: "; cin >> p;
+            cout << "Email: "; cin >> e;
+            collaborators.push_back(Collaborator(n, p, e));
+            cout << "Collaborator added\n";
+        }
+        else if (op == 3) {
+            string n, d;
+            cout << "Project name: "; cin >> n;
+            cout << "Due date: "; cin >> d;
+            projects.push_back(Project(n, d));
+        }
+        else if (op == 4) {
+            int idxProj;
+            cout << "Project index (0-" << projects.size()-1 << "): ";
+            cin >> idxProj;
+            int miembro;
+            cout << "Add 1. Admin or 2. Collaborator "; cin >> miembro;
+            if (miembro == 1) {
+                for (int i = 0; i < admins.size(); i++)
+                    cout << i << ". " << admins[i].getName() << endl;
+                int idxA;
+                cout << "Admin index: "; 
+                cin >> idxA;
+                projects[idxProj].addMember(&admins[idxA]);
+            }
+            else {
+                for (int i = 0; i < collaborators.size(); i++)
+                    cout << i << ". " << collaborators[i].getName() << endl;
+                int idxC;
+                cout << "Collaborator index: "; cin >> idxC;
+                projects[idxProj].addMember(&collaborators[idxC]);
+            }
+        }
+        else if (op == 5) {
+            int idxProj, idxA;
+            cout << "Project index (0-" << projects.size()-1 << "): ";
+            cin >> idxProj;
+            cout << "Admin index (0-" << admins.size()-1 << "): ";
+            cin >> idxA;
+            string tn, td, ta;
+            int th;
+            cout << "Task name: "; cin >> tn;
+            cout << "Description: "; cin >> td;
+            cout << "Assigned to: "; cin >> ta;
+            cout << "Estimated hours: "; cin >> th;
+            Task t = admins[idxA].createTask(tn, td, ta, th);
+            projects[idxProj] += t;
+            cout << "Task added to project\n";
+        }
+        else if (op == 6) {
+            for (int i = 0; i < projects.size(); i++) {
+                cout << "Project " << i << ": " << projects[i].getName() << ", Due: " << projects[i].getDueDate() << endl;
+                cout << "Members: ";
+                for (int j = 0; j < projects[i].getMembers().size(); j++)
+                    cout << projects[i].getMembers()[j]->getName() << " | ";
+                cout << endl << "Tasks:\n";
+                for (int k = 0; k < projects[i].getTasks().size(); k++)
+                    cout << " " << k << ". " << projects[i].getTasks()[k].getName()
+                         << " [" << projects[i].getTasks()[k].getState() << "] Assigned to: "
+                         << projects[i].getTasks()[k].getAssignedTo() << endl;
+            }
+        }
     }
     return 0;
 }
+
