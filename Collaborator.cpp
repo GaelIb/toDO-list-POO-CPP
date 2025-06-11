343b4ddf0a5f5f677c350254e90767882c75ea90
GaelIb <ibayezgael@gmail.com>
2025-06-10 21:32:56 -0600

    Update Collaborator.cpp

--- Collaborator.cpp
+++ Collaborator.cpp
@@ -2,9 +2,8 @@
 
 Collaborator::Collaborator() {}
 Collaborator::Collaborator(string n, string p, string e) : User(n, p, e) {}
-
+// Show role implementation
 void Collaborator::showRole() {
-    // Do nothing here, print in main
 }
 void Collaborator::changeTaskState(Task* t, string newState) {
     if (t) t->changeState(newState);
