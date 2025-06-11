fcf66460e647e9703241cad380ea1bac54a7612f
GaelIb <ibayezgael@gmail.com>
2025-06-10 21:31:15 -0600

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -3,12 +3,14 @@
 
 #include "User.h"
 #include "Task.h"
+// Derived class: Admin inherits from User
 
 class Admin : public User {
 public:
     Admin();
     Admin(string, string, string);
 
+    // Implementation of pure virtual method
     void showRole(); // override
 
     Task createTask(string, string, string, int);
